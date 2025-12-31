401de290ae44fc8ef782ac8b5057e12ea8721763
Gabrick75 <gabriel.dijair.souza@gmail.com>
2025-12-31 19:06:50 -0300

    Exercise completed: Image and texts positioned correctly

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,9 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +43,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingImage(modifier: Modifier = Modifier) {
+fun GreetingImage() {
     // Carrega a imagem do diretório drawable
     val image = painterResource(id = R.drawable.bg_compose_background)
 
